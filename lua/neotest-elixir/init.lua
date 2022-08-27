43abbff63d2d3a83e067e569ee26f70873a1834c
Jhon Pedroza <jhon@pedroza.me>
2022-08-27 03:50:30 -0500

    feat: build test command

--- lua/neotest-elixir/init.lua
+++ lua/neotest-elixir/init.lua
@@ -1,8 +1,30 @@
+local Path = require("plenary.path")
 local lib = require("neotest.lib")
 local base = require("neotest-elixir.base")
 
 ---@type neotest.Adapter
 local ElixirNeotestAdapter = { name = "neotest-elixir" }
+
+---@param position neotest.Position
+---@return string[]
+local function get_args(position)
+  if position.type == "dir" then
+    local root = ElixirNeotestAdapter.root(position.path)
+    local path = Path:new(position.path)
+    local relative = path:make_relative(root)
+
+    if relative == "." then
+      return {}
+    else
+      return { relative }
+    end
+  elseif position.type == "file" then
+    return { position.path }
+  else
+    local line = position.range[1] + 1
+    return { position.path .. ":" .. line }
+  end
+end
 
 ElixirNeotestAdapter.root = lib.files.match_root_pattern("mix.exs")
 
@@ -10,6 +32,8 @@ function ElixirNeotestAdapter.is_test_file(file_path)
   return base.is_test_file(file_path)
 end
 
+---@async
+---@return neotest.Tree | nil
 function ElixirNeotestAdapter.discover_positions(path)
   local query = [[
   ;; Describe blocks
@@ -30,14 +54,28 @@ function ElixirNeotestAdapter.discover_positions(path)
   return lib.treesitter.parse_positions(path, query, { nested_namespaces = false })
 end
 
+---@async
+---@param args neotest.RunArgs
+---@return neotest.RunSpec
 function ElixirNeotestAdapter.build_spec(args)
-  print(vim.inspect(args))
+  print("Called build_spec")
+
+  local position = args.tree:data()
+  local command = vim.list_extend({ "mix", "test" }, get_args(position))
+  print("Position", vim.inspect(command))
+
+  return {
+    command = command,
+  }
 end
 
-function ElixirNeotestAdapter.results(spec, result, tree)
-  print(vim.inspect(spec))
+---@async
+---@param spec neotest.RunSpec
+---@param result neotest.StrategyResult
+---@return neotest.Result[]
+function ElixirNeotestAdapter.results(spec, result)
   print(vim.inspect(result))
-  print(vim.inspect(tree))
+  vim.cmd("vsplit " .. result.output)
 end
 
 return ElixirNeotestAdapter
